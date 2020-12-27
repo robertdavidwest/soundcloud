8121c70746cc0833a2a0a19c24d769e3c5bc0c1e
Robert West <robert.david.west@gmail.com>
2020-12-27 18:46:44 +0000

    Added user data pull to api suite

--- src/soundcloud/api.clj
+++ src/soundcloud/api.clj
@@ -4,16 +4,28 @@
   (:require [clj-http.client :as client])
   (:require [clojure.edn :as edn])
   (:require [clojure.string :as str])
-  ;(:require [net.cgrand.enlive-html :as html])
-  )
-;(import [java.net URL])
+)
 
+
+(def base-url "https://api-v2.soundcloud.com/users/")
+
+(defn get-user-url
+  [user-num client-id]
+  (str base-url user-num "?client_id=" client-id))
 
 (defn get-tracks-url
   [user-num client-id]
-  (let [base-url "https://api-v2.soundcloud.com/users/"
-        url (str base-url user-num "/tracks?client_id=" client-id)]
-    url))
+  (str base-url user-num "/tracks?client_id=" client-id))
+
+(defn get-user-info
+  [user-num client-id]
+  (let [url (get-user-url user-num client-id) 
+        response (client/get url)
+        status-code (:status response)
+        body (:body response)
+        _json (json/read-str body)
+        ]
+    _json))
 
 (defn get-tracks
   [user-num client-id]
@@ -24,6 +36,4 @@
         _json (json/read-str body)
         collection (get _json "collection")
         ]
-    (comment (when (= status-code 200)
-               tracks))
     collection))
